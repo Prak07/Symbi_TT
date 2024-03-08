dc520974b3bb56e07c19aaa1cabd4229c0d8baa3
Prakhar <tarangagrawal@gmail.com>
2024-03-08 16:43:44 +0530

    resolved footer js in each page by adding base js file

--- tt/static/js/contact.js
+++ tt/static/js/contact.js
@@ -87,30 +87,3 @@ gsap.from("#contact-info .elem-cont h1, #info .elem-cont h3", {
   delay: 1,
 });
 
-gsap.from("#footer #footer-container", {
-  opacity: 0,
-  duration: 1,
-  delay: 0.5,
-  scrollTrigger: {
-    trigger: "#footer #footer-container",
-    scroller: "#main",
-    start: "top 97%",
-  },
-});
-
-gsap.from(".links h3 ", {
-  y: 200,
-  opacity: 0,
-  duration: 1.3,
-  delay: 0.1,
-  scrollTrigger: {
-    trigger: ".links h3",
-    scroller: "#main",
-    start: "top 96%",
-  },
-});
-
-Shery.makeMagnet("#nav-1 a,.links a", {
-  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  duration: 1,
-});
