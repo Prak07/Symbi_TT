f6a4889b334e6b708ca85b6b0998064b241de6ce
Prakhar <tarangagrawal@gmail.com>
2024-03-21 18:09:15 +0530

    data is now showingggg

--- tt/views.py
+++ tt/views.py
@@ -6,75 +6,9 @@ from .models import *
 from django.contrib import messages
 from django.contrib.auth.hashers import make_password
 from django.core.validators import validate_email
-
-
-def update_profile(request, page):
-    if request.method == "POST":
-        user = request.user
-        div = request.POST["div"]
-        email = request.POST["email"]
-        div = div.upper()
-        sem = request.POST["sem"]
-        sem = sem.upper()
-        sem_s = {
-            "1": "I",
-            "2": "II",
-            "3": "III",
-            "4": "IV",
-            "5": "V",
-            "6": "VI",
-            "7": "VII",
-            "8": "VIII",
-        }
-        if sem in sem_s:
-            sem = sem_s[sem]
-        program = request.POST["program"]
-        elective = request.POST["elective"]
-        Honours = request.POST["honours"]
-        if Honours == "yes":
-            user.email = email
-            user.sem = sem
-            user.div = div
-            user.program = program
-            user.elective = elective
-            user.honours = True
-            user.save()
-            messages.info(request,"Profile Updated")
-        else:
-            user.email = email
-            user.sem = sem
-            user.div = div
-            user.program = program
-            user.honours = False
-            user.elective = elective
-            user.save()
-            messages.info(request,"Profile Updated")
-    return render(request, page)
-
-
-def contact(request):
-    if request.method == "POST" and "contact_form" in request.POST:
-        name = request.POST.get("name")
-        email = request.POST.get("email")
-        message = request.POST.get("message")
-        entry = ContactUser.objects.create(name=name, email=email, message=message)
-        entry.save()
-        messages.info(request, "Message Sent")
-        return redirect("contact")
-    update_profile(request, "contact.html")
-    return render(request, "contact.html")
-
-
-def home(request):
-    update_profile(request, "index.html")
-    return render(request, "index.html")
-
-
-def about(request):
-    update_profile(request, "about.html")
-    return render(request, "about.html")
-
-
+import requests
+from bs4 import BeautifulSoup
+from datetime import date , datetime
 def login(request):
     if request.method == "POST":
         submit_action = request.POST.get("submit_action")
@@ -107,7 +41,7 @@ def login(request):
                     if sem in sem_s.values():
                         if password == confirm_password:
                             if CustomUser.objects.filter(email=email).exists():
-                                messages.info(request, "Username already taken")
+                                messages.info(request, "Email already taken")
                                 return render(
                                     request,
                                     "login.html",
@@ -134,13 +68,8 @@ def login(request):
                                         honours=True,
                                     )
                                     CustomUser.save
-                                    messages.success(
-                                        request,
-                                        "Registration done now sign in to continue",
-                                    )
-                                    return render(
-                                        request, "login.html", {"color": "green"}
-                                    )
+                                    auth.login(request, user)
+                                    return redirect("/")
                                 else:
                                     user = CustomUser.objects.create(
                                         username=email,
@@ -153,13 +82,8 @@ def login(request):
                                         honours=False,
                                     )
                                     CustomUser.save
-                                    messages.success(
-                                        request,
-                                        "Registration done now sign in to continue",
-                                    )
-                                    return render(
-                                        request, "login.html", {"color": "green"}
-                                    )
+                                    auth.login(request, user)
+                                    return redirect("/")
                         else:
                             messages.info(request, "Password dosent match")
                             return render(
@@ -243,3 +167,243 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect("/")
+
+
+def update_profile(request, page):
+    if request.method == "POST":
+        user = request.user
+        div = request.POST["div"]
+        email = request.POST["email"]
+        div = div.upper()
+        sem = request.POST["sem"]
+        sem = sem.upper()
+        sem_s = {
+            "1": "I",
+            "2": "II",
+            "3": "III",
+            "4": "IV",
+            "5": "V",
+            "6": "VI",
+            "7": "VII",
+            "8": "VIII",
+        }
+        if sem in sem_s:
+            sem = sem_s[sem]
+        program = request.POST["program"]
+        elective = request.POST["elective"]
+        Honours = request.POST["honours"]
+        if Honours == "yes":
+            user.email = email
+            user.sem = sem
+            user.div = div
+            user.program = program
+            user.elective = elective
+            user.honours = True
+            user.save()
+            messages.info(request,"Profile Updated")
+        else:
+            user.email = email
+            user.sem = sem
+            user.div = div
+            user.program = program
+            user.honours = False
+            user.elective = elective
+            user.save()
+            messages.info(request,"Profile Updated")
+    return render(request, page)
+
+
+
+def home(request):
+    update_profile(request, "index.html")
+    data=routine(request)
+    return render(request, "index.html",{"data":data})
+
+def about(request):
+    update_profile(request, "about.html")
+    return render(request, "about.html")
+
+
+def contact(request):
+    if request.method == "POST" and "contact_form" in request.POST:
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+        entry = ContactUser.objects.create(name=name, email=email, message=message)
+        entry.save()
+        messages.info(request, "Message Sent")
+        return redirect("contact")
+    update_profile(request, "contact.html")
+    return render(request, "contact.html")
+
+def routine(request):
+    if request.user.is_authenticated:
+        today=str(date.today())
+        year,month,day=today.split("-")
+        url=f"http://time-table.sicsr.ac.in/day.php?year={year}&month={month}&day={day}&area=1&room=29"
+        course=request.user.program
+        sem=request.user.sem
+        div=request.user.div
+        honours=request.user.honours
+        elective=request.user.elective
+        def fetch(url,path):
+            r=requests.get(url)
+            with open(path,"w") as f:
+                f.write(r.text)
+        try:
+            fetch(url,"scrap.html")
+            with open('scrap.html') as f:
+                html_doc=f.read()
+        except:
+            pass
+
+        soup=BeautifulSoup(html_doc,"html.parser")
+        l=[]
+        def scraping(i):
+                    try:
+                        url="http://time-table.sicsr.ac.in/"+i.get("href")
+                        fetch(url,"scrap2.html")
+                        with open('scrap2.html') as f:
+                            html_doc1=f.read()
+                            soup1=BeautifulSoup(html_doc1,"html.parser")
+
+                            tds=soup1.find_all("td")
+                            
+                            if "Extra Lecture" in tds[1].get_text() or "Lecture Scheduled" in tds[1].get_text():
+                                subject=tds[1].get_text().split(":")
+                                subject="Extra Lecture : "+subject[2]
+                            elif ":" in tds[1].get_text():
+                                subject=tds[1].get_text().split(":")
+                                subject=subject[1].split("-")
+                                subject=subject[0]
+                            else:
+                                subject=tds[1].get_text().split("-") 
+                                subject=subject[1].replace(sem,"")
+
+                            room=str(tds[5].get_text().split("-")[1])
+
+                            start=tds[7].get_text().split("-")[0]
+                            time_obj = datetime.strptime(start.strip(), "%H:%M:%S")
+                            start = str(time_obj.strftime("%I:%M:%S "))
+
+                            duration=tds[9].get_text()
+
+                            end=str(tds[11].get_text().split("-")[0])
+                            time_obj = datetime.strptime(end.strip(), "%H:%M:%S")
+                            end = str(time_obj.strftime("%I:%M:%S "))
+
+                            l.append([str(subject),start,end,room])
+                    except Exception as e:
+                        print(e)
+        ele=["Basic Psychology","Foundations of Ethics","Understanding Cinema","Appreciating Cinema","Music in Media","Introduction to Theatre","Basic Sociology","Creative Writing"]
+        def electives(elective):
+            if elective=="Basic Psychology 1":
+                if  ele[0] in title and "1" in title:
+                    scraping(i) 
+            elif elective=="Basic Psychology 2":   
+                if  ele[0] in title and "2" in title:
+                    scraping(i)
+            elif elective=="Basic Psychology 3":    
+                if  ele[0] in title and "3" in title:
+                    scraping(i)
+            elif elective=="Foundations":
+                if ele[1] in title:
+                    scraping(i)
+            elif elective=="Understanding Cinema":        
+                if  ele[2] in title :
+                    scraping(i)
+            elif elective=="Appreciating":
+                if  ele[3] in title:
+                    scraping(i)
+            elif elective=="Music":
+                if  ele[4] in title :
+                    scraping(i)
+            elif elective=="Theatre":
+                if  ele[5] in title:
+                    scraping(i)
+            elif elective=="Sociology":
+                if  ele[6] in title:
+                    scraping(i)
+            elif elective=="Creative":
+                if  ele[7] in title:
+                    scraping(i)
+            if elective == "other":
+                if "Elective" in title:
+                    if not any(e in title for e in ele):
+                        scraping(i)
+        for i in soup.find_all("a"):
+            raw_title=str(i.get("title")).split()
+            title=" ".join(raw_title)
+            if course=="BCA":
+                if "BCA" in title :
+                    if f"{sem} " in title or f"{sem}-" in title :
+                        if f" {div} " in title or f" {div}-" in title or f" {div}:" in title :
+                            scraping(i)
+                        if honours=="yes":
+                            if "Honours" in title:
+                                scraping(i)
+                        if "Flexi-Credit" in title:
+                            scraping(i)
+                        if elective!="none":
+                            electives(elective)
+                            
+            elif course=="BBA(IT)":
+                if "BBA(IT)" in title:
+                    if f"{sem} " in title or f"{sem}-" in title :
+                        if f" {div} " in title or f" {div}-" in title or f" {div}:" in title :
+                            scraping(i)
+                        if "Flexi-Credit" in title:
+                            scraping(i)
+                        if elective!="none":
+                            electives(elective)
+            elif course=="MBA(IT)":
+                if "MBA(IT)" in title:
+                    if f"{sem} " in title or f"{sem}-" in title :
+                            if f" {div} " in title or f" {div}-" in title or f" {div}:" in title :
+                                scraping(i)
+                    if "(SPM)" in title or "(DA)" in title:
+                        scraping(i)
+                    if "Flexi-Credit" in title:
+                        scraping(i)
+                    if elective!="none":
+                        electives(elective)
+            elif course=="MSC(CA)":              
+                if "MSC(CA)" in title:
+                    if f"{sem} " in title or f"{sem}-" in title :
+                        scraping(i)
+                    if "Flexi-Credit" in title:
+                        scraping(i)
+                    if elective!="none":
+                        electives(elective)
+            elif course=="MBA(DT)":              
+                if "MBA(DT)" in title:
+                    if f"{sem} " in title or f"{sem}-" in title :
+                        scraping(i)
+                    if "Flexi-Credit" in title:
+                        scraping(i)
+                    if elective!="none":
+                        electives(elective)
+
+        j=0
+        data=[]
+        for i in l:
+            try:
+                if l[j][2]==l[j+1][1] and l[j][0]==l[j+1][0]:
+                    data.append([str(l[j][0]),str(l[j][1]),str(l[j+1][2]),str(l[j][3])])
+                    j+=2
+                else:
+                    data.append([str(l[j][0]),l[j][1],l[j][2],l[j][3]])
+                    j+=1
+            except IndexError:
+                try:
+                    data.append([l[j][0],l[j][1],l[j][2],l[j][3]])
+                    break
+                except IndexError:
+                    break
+                
+        if data!=[]:
+            return data
+        else:
+            return []
+    else:
+        return "none"
