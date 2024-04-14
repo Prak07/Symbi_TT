fcc76c9aeb01bad2f8ee4dad340bbedeb36c2409
Prak07 <tarangagrawal768@gmail.com>
2024-04-15 04:09:55 +0530

    changes made for azure deployment

--- time_table/urls.py
+++ time_table/urls.py
@@ -7,7 +7,7 @@ from django.views.static import serve
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("tt.urls")),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 handler404='tt.views.error_404'
