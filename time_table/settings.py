45e103ffd261c309da84eb15fd1444696270cadd
Prak07 <tarangagrawal768@gmail.com>
2024-04-15 01:55:11 +0530

    changes made for azure deployment

--- time_table/settings.py
+++ time_table/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG")
+DEBUG = True
 
 ALLOWED_HOSTS = ["*","symbi-tt.azurewebsites.net"]
 CSRF_TRUSTED_ORIGINS=["https://"+"symbi-tt.azurewebsites.net"]
