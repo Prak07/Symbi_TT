f6793fdc5f36faa4bdb1a9c837b95878a6ad7740
Prak07 <tarangagrawal768@gmail.com>
2024-04-15 05:30:47 +0530

    changes made for azure deployment

--- time_table/settings.py
+++ time_table/settings.py
@@ -96,10 +96,12 @@ USE_TZ = True
 
 
 
-STATIC_URL = '/static/'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('azure_content/static')),)
+STATIC_URL = '/azure_content/static/'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
