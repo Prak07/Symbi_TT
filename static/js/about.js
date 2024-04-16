738f8718cc48d6fdc28e1e27e68dcc3433deafc8
prakhar <113136357+Prak07@users.noreply.github.com>
2024-04-16 15:04:06 +0530

    Revert "founders section added and some more tweaks done"

--- static/js/about.js
+++ static/js/about.js
@@ -76,24 +76,63 @@ gsap.from("#nav-1 a", {
   stagger: 0.1,
 });
 
-gsap.from("#founders .content h1", {
+gsap.from("#objective .content h1", {
   y: 100,
   opacity: 0,
   duration: 1,
   scrollTrigger: {
-    trigger: "#founders .content h1",
+    trigger: "#objective .content h1",
     scroller: "#main",
     start: "top 90%",
   },
 });
-gsap.from("#founders .underline", {
+gsap.from("#objective .underline", {
   x: 200,
   opacity: 0,
   duration: 0.7,
   scrollTrigger: {
-    trigger: "#founders .underline",
+    trigger: "#objective .underline",
     scroller: "#main",
     start: "top 90%",
   },
 });
-  
+gsap.from("#objective p", {
+  opacity: 0,
+  duration: 0.7,
+  scrollTrigger: {
+    trigger: "#objective p",
+    scroller: "#main",
+    start: "top 70%",
+  },
+});
+
+gsap.from("#scope h1", {
+  y: 100,
+  opacity: 0,
+  duration: 1,
+
+  scrollTrigger: {
+    trigger: "#scope h1",
+    scroller: "#main",
+    start: "top 95%",
+  },
+});
+gsap.from("#scope .underline", {
+  x: -200,
+  opacity: 0,
+  duration: 0.7,
+  scrollTrigger: {
+    trigger: "#scope .underline",
+    scroller: "#main",
+    start: "top 100%",
+  },
+});
+gsap.from("#scope p", {
+  opacity: 0,
+  duration: 0.7,
+  scrollTrigger: {
+    trigger: "#scope p",
+    scroller: "#main",
+    start: "top 70%",
+  },
+});
--- tt/views.py
+++ tt/views.py
@@ -498,6 +498,5 @@ def update_routine(request):
 
         return asyncio.run(async_home())
 
-
 def error_404(request, exception):
-    return render(request, "404.html")
+    return render(request,"404.html")
