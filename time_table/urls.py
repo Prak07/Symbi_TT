f019ecf51d475f024e577db2b407d2d4409d253b
Prak07 <tarangagrawal768@gmail.com>
2024-04-17 01:35:13 +0530

    changed something

--- time_table/urls.py
+++ time_table/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.views.static import serve
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin_symbi_tt/", admin.site.urls),
     path("", include("tt.urls")),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
